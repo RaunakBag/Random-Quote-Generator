3250660cd9a31d8a5849c124e25acfd13b1bb66c
rishipurwar1 <rishipurwar007@gmail.com>
2020-12-24 13:18:58 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-const text = document.querySelector('.text');
+const text = document.querySelector('.quote');
 const auth = document.querySelector('.author');
-const shareBtn = document.getElementById('share-button');
-const buttons = document.querySelector('.buttons');
+const button = document.querySelector('.twitter-share-button');
+const nextBtn = document.querySelector('.next');
+const loader = document.querySelector('.loader');
 
-async function getQuotes() {
-    const res = await fetch(
-        `https://type.fit/api/quotes`
-    );
-
-    const data = await res.json();
-
-    return data;
-}
-
-async function getQuote() {
-    const quotes = await getQuotes();
+const getQuote = async () => {
+    loader.classList.remove('hide');
+    const res = await fetch(`https://type.fit/api/quotes`);
+    const quotes = await res.json();
+    loader.classList.add('hide');
     const num = Math.floor(Math.random()*quotes.length);
-    const quoteOb = quotes[num];
-    const quote = quoteOb.text;
-    const author = quoteOb.author;
+    const item = quotes[num];
+    const quote = item.text;
+    const author = item.author;
     text.innerText = quote;
     auth.innerText = author;
-    buttons.innerHTML = `
-    <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=${quote}" rel="noopener noreferrer"><img src="twitter.svg" alt=""></a>
-    <button class="next">Next</button>
-    `;
-    document.querySelector('.next').addEventListener('click', getQuote);
+    button.href = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
 }
+
+nextBtn.addEventListener('click', getQuote);
 
 getQuote();
